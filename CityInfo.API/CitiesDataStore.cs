ebe8614034ebca16976cc0cfdf733e9afea1ffbe
Unknown <fvasconcelos@stone.com.br>
2018-04-09 11:21:35 -0300

    Start

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "Rio de Janeiro",
+                    Description = "Morre-se rapidamente",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Cristo Redentor",
+                            Description = "Ótimo local para assaltos"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2, 
+                            Name = "Rocinha",
+                            Description = "Biggest favela in the world"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "São Paulo",
+                    Description = "Não muito diferente",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 3,
+                            Name = "...",
+                            Description = "Difícil achar um..."
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ CityInfo.API/CityInfoContextExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityInfo.API.Entities;
+
+namespace CityInfo.API
+{
+    public static class CityInfoContextExtensions
+    {
+        public static void EnsureSeedDataForContext(this CityInfoContext context)
+        {
+            if (context.Cities.Any())
+            {
+                return;
+            }
+
+            var cities = new List<City>()
+            {
+                new City()
+                {
+                    Name = "Rio de Janeiro",
+                    Description = "Cidade maravilhosa",
+                    PointOfInterest = new List<PointOfInterest>()
+                    {
+                        new PointOfInterest()
+                        {
+                            Name = "Cristo Redentor",
+                            Desciption = "Aquele famoso no mundo inteiro"
+                        },
+                        new PointOfInterest()
+                        {
+                            Name = "Pão de açucar",
+                            Desciption = "Aquele dos bondinhos"
+                        }
+                    }
+                },
+                new City()
+                {
+                    Name = "São Paulo",
+                    Description = "Terra da garoa",
+                    PointOfInterest = new List<PointOfInterest>()
+                    {
+                        new PointOfInterest()
+                        {
+                            Name = "Ponte Octávio Frias de Oliveira",
+                            Desciption =
+                                "A única ponte estaiada do mundo com duas pistas em curva conectadas a um mesmo mastro"
+                        }
+                    }
+                }
+            };
+
+            context.Cities.AddRange(cities);
+            context.SaveChanges();
+        }
+    }
+}
